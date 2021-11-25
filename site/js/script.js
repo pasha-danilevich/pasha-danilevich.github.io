1a253f3d2233bfe1603453af51af3364b3d92f25
pasha-danilevich <pasha-danilevich@outlook.com>
2021-11-25 22:54:18 +0300

    menu

--- site/js/script.js
+++ site/js/script.js
@@ -32,6 +32,65 @@ $(document).ready(function(){
   });
 });
 
+
+var checkbox =  $('#menu-checkbox');
+function checkboxClose() {
+  document.querySelector('#menu-checkbox').checked = false;
+  $('#menuToggle').css({'top': + -32 + 'px'});
+  menuVoidBack.css({"width": 0 + 'px'})
+  menuVoidBack.css({"height": 0 + 'px'})
+}
+
+const pageWidth = document.documentElement.scrollWidth
+const windowInnerHeight = window.innerHeight
+var menuVoidBack = $('.menu-void-back')
+
+
+$(function () {
+  checkbox.click(function () {
+    if (this.checked) {
+      $('#menuToggle').css({'top': + -30 + 'px'});
+      menuVoidBack.css({"width": pageWidth})
+      menuVoidBack.css({"height": windowInnerHeight})
+      
+      $(function () {
+        menuVoidBack.click(function () {
+          checkboxClose()
+        })
+      });
+
+    }
+    else {
+      checkboxClose()
+    }
+  })
+});
+if(pageWidth < 500){
+  $('#menu').css({"width": pageWidth + 70})
+}
+
+$(window).scroll(function(e) {      
+    if (window.pageYOffset > 200){
+  checkboxClose()
+}
+});
+
+// window.addEventListener('resize', function(event) {
+//   menuVoidBack.css({"width": pageWidth})
+//   menuVoidBack.css({"height": windowInnerHeight})
+//   console.log('fff')
+// }, true);
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 600);
+    });
+});
+
+
 window.onload = function(event) {
   console.log("load");
 }
