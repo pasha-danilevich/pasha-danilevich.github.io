11b2add9111dad5cdcdf4c28af9b76e845fc88b5
pasha-danilevich <pasha-danilevich@outlook.com>
2022-02-19 18:17:40 +0300

    10

--- site/js/add-picture.js
+++ site/js/add-picture.js
@@ -1,56 +1,79 @@
+$('#add_picture').on('click',function(){
+  var list = document.getElementById("hashtag");
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+  $('#hat').addClass('hat-active');
+  $('body').css('overflow', 'hidden')
+  render(result_picture_name, picture_name.value)
+  priceCalculation(picture_price.value)
+  sizeRender()
+  catalogRander(listName)
+})
+$('#close_hat_button').on('click',function(){
+  $('#hat').removeClass('hat-active')
+  $('body').css('overflow', 'scroll')
+  $('body,html').animate({scrollTop: 0}, 400); 
+})
+
+// --------------------------------------------------
+
+
+function render(name, value) {
+    name.innerHTML = value;
+    let liLast = document.createElement('li');
+    liLast.innerHTML = value;
+    $('#hashtag').append(liLast);
+}
+function sizeRender() {
+  let size = [0,0]
+  
+  size[0] = picture_height.value
+  size[1] = picture_width.value
+  sizeСalculation(size[0],size[1])
+  result_picture_size.innerHTML = size[0] + ' х ' + size[1] + ' см'
+}
 function sizeСalculation(height, width) {
   let all_size = Number(height) + Number(width)
   
-  if (all_size < 80){
-    console.log(all_size)
-    result_picture_size_tag.innerHTML = "Маленькая"
+  if (all_size <= 80){
+    render(result_picture_size, "Маленькая")
   }
   if (all_size > 80 && all_size < 180){
-    result_picture_size_tag.innerHTML = "Средняя"
+    render(result_picture_size, "Средняя")
   }
-  if (all_size > 180){
-    result_picture_size_tag.innerHTML = "Большая"
-  }
-  if (all_size <= 0){
-    result_picture_size_tag.style.display = 'none'
-  }else {
-    result_picture_size_tag.style.display = 'block'
+  if (all_size >= 180){
+    render(result_picture_size, "Большая")
   }
 }
 function priceCalculation(argument) {
   let loc_price = Number(argument).toLocaleString()
   let price = Number(argument)
-  console.log(price)
+  
   if (price < 10000){
-    result_picture_price_tag.innerHTML = 'До 10 000₽'
+    render(result_picture_price, 'До 10 000₽')
   }else if(price < 100000){
-    result_picture_price_tag.innerHTML = '10 000₽ - 100 000₽'
-    
+    render(result_picture_price, '10 000₽ - 100 000₽')
   }
   if(price > 100000){
-    result_picture_price_tag.innerHTML = 'Более 100 000₽'
+    render(result_picture_price, 'Более 100 000₽')
   }
-  if(price <= 0) {
-    result_picture_price_tag.style.display = 'none'
-  }else {
-    result_picture_price_tag.style.display = 'block'
-  }
-
-  
   result_picture_price.innerHTML = loc_price + " ₽";
-  
-  
+}
+function catalogRander(list) {
+  var lenList = list.length
+  console.log(lenList)
+  for (let i = 0; i < lenList; i++){
+    let liLast = document.createElement('li');
+    console.log(list[i])
+    liLast.innerHTML = list[i];
+    $('#hashtag').append(liLast);
+  }
+    
 }
 
-function render(name, name_tag, value) {
-    name.innerHTML = value;
-     let liLast = document.createElement('li');
-    liLast.innerHTML = value;
-    $('#hashtag').append(liLast);
-    // name_tag.innerHTML = value;
-    // name_tag.style.display = 'block'
-}
-// --------------------------------------------------
+
+// -----------Catalog---------------------------------------
 
 var listName = [];
 
@@ -72,25 +95,21 @@ $('.form-item_checkbox input').change(function(e) {
   }
 });
 
-// --------------------------------------------------
 
-picture_name.oninput = function() {
-    render(result_picture_name,result_picture_tag, picture_name.value)
-}
 
-picture_price.oninput = function() {
-  priceCalculation(picture_price.value)
-}
 
-let size = [0,0]
-picture_height.oninput = function() {
-    size[0] = picture_height.value
-    result_picture_size.innerHTML = size[0] + ' х ' + size[1] + ' см'
-    sizeСalculation(size[0],size[1])
-} 
-picture_width.oninput = function() {
-    size[1] = picture_width.value
-    result_picture_size.innerHTML = size[0] + ' х ' + size[1] + ' см'
-    sizeСalculation(size[0],size[1])
-} 
 
+
+// -----------------Scroll---------------------------------
+
+var 
+    bar = $('#bar'),
+    $window = $(window),
+    docHeight = $(document).height(),
+    winHeight = $window.height(),
+    baseX = docHeight - winHeight;
+
+$window.scroll(function(e) {      
+    var x = ($window.scrollTop() / baseX) * 110;
+    bar.css({'width': + x + '%'});
+});
